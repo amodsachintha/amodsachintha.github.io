86eb894d77c9c9e85b5eda5f5e85e95672e33820
amodsachintha <amod077@gmail.com>
2018-08-09 13:54:40 +0530

    amodsachintha.wordpress.com imported to Jekyll site :sunglasses:

---
+++ import.rb
@@ -0,0 +1,6 @@
+require "jekyll-import";
+    JekyllImport::Importers::WordpressDotCom.run({
+      "source" => "wordpress.xml",
+      "no_fetch_images" => false,
+      "assets_folder" => "assets"
+    })
